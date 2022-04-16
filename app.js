411cf872659fa11df9f6e2ca88117dfe0be81e85
Jonah Milner <jonahmilner86@gmail.com>
2022-04-15 18:51:11 -0600

    fixed a bunch of redundant, unecessary files, i thought we were building a server for some reason

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const inquirer = require('inquirer');
-const express = require('express');
-const PORT = process.env.PORT || 3001;
 const db = require('./db/connection');
 const { listenerCount } = require('process');
-const app = express();
+const table = require('console.table');
 
 
 
@@ -11,9 +9,10 @@ const init = () => {
     const options = () => {
         inquirer.prompt([
             {
-                type: list,
-                name: 'action',
-                choices: [
+                type: 'list',
+                name: 'options',
+                choices:
+                [
                     'View all departments',
                     'View all roles', 
                     'View all employees',
@@ -23,11 +22,139 @@ const init = () => {
                     'Update an employee role' 
                 ]
             }
-        ]).then(
-            function(result) {
-                
-            })
+        ]).then(function(result) {
+            switch (result.options) {
+                case 'View all departments':
+                    viewDepartments();
+                    break;
+                case 'View all roles':
+                    viewRoles();
+                    break;
+                case 'View all employees':
+                    viewEmployees();
+                    break;
+                case 'Add a department':
+                    addDepartment();
+                    break;
+                case 'Add a role':
+                    addRole();
+                    break;
+                case 'Add an employee':
+                    addEmployee();
+                    break;
+                case 'Update an employee role':
+                    updateRole();
+                    break
+            }
+        })
     }
+    options();
 };
+
+
+const viewDepartments = () => {
+    const sql = `SELECT * FROM departments`;
+    db.query(sql, (err, rows) => {
+        if(err) {
+            console.log(err.message);
+            return;
+        }
+        console.table(rows);
+        // restart();
+    });
+};
+
+const viewEmployees = () => {
+    const sql = `SELECT * FROM employees`;
+    db.query(sql, (err, rows) => {
+        if(err) {
+            console.log(err.message);
+            return;
+        }
+        console.table(rows);
+        // restart();
+    });
+};
+
+const viewRoles = () => {
+    const sql = `SELECT * FROM roles`;
+    db.query(sql, (err, rows) => {
+        if(err) {
+            console.log(err.message);
+            return;
+        }
+        console.table(rows);
+        // restart();
+    });
+};
+
+const addDepartment = () => {
+        inquirer.prompt({
+        type: 'input',
+        message: 'Please add department name'
+    })
+    .then(function (result) {
+        db.query(`INSERT INTO  departments ?`, result, (req, res) => {
+            console.table(res)
+        })
+    })
+};
+
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Please enter employee first name'
+        },
+        {
+            type: 'input',
+            message:'Please enter employee last name'
+        },
+        {
+            type: 'input',
+            message:'Please enter employee role'
+        },
+        {
+            type: 'input',
+            message: 'Please enter employee manager ID'
+        }
+    ])
+    .then(function (result) {
+        db.query(`INSERT INTO  employees ?`, result, (req, res) => {
+            console.table(res)
+        })
+    })
+};
+
+const addRole = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: 'Please add role'
+        },
+        {
+            input: 'input',
+            message: 'Please input the salary for this role'
+        },
+        {
+            type: 'input',
+            message: 'Please enter Department ID for role'
+        }
+            
+    ])
+    .then(function (result) {
+        db.query(`INSERT INTO  roles ?`, result, (req, res) => {
+            console.table(res)
+        })
+    })
+};
+
+const updateRole = () => {
+    inquirer.prompt({
+        type: 'choices',
+        message: 'Please update employee role'
+    })
+};
+
 
 init();
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,11 +1,11 @@
-INSERT INTO departments(title)
+INSERT INTO departments (title)
 VALUES 
 ('Rock Band');
 
-INSERT INTO roles(title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES
 ('Singer', 20000000, 1);
 
-INSERT INTO employees(first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
 ('Robert', 'Plant', 1, 1);
